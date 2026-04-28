ea9867ea9ed67451fc7e1ee2f37904045eea3850
Iqra-qazi <iqraq577@gmail.com>
2026-04-28 23:26:09 +0500

    js cv

--- app.js
+++ app.js
@@ -1,72 +1,89 @@
 
 
-  function signup() {
-    var name = document.getElementById("signupName").value;
-    var email = document.getElementById("signupEmail").value;
-    var password = document.getElementById("signupPassword").value;
+function signup() {
+  var name = document.getElementById("signupName").value;
+  var email = document.getElementById("signupEmail").value;
+  var password = document.getElementById("signupPassword").value;
 
-    var user = {
-      name: name,
-      email: email,
-      password: password
-    };
+  var user = {
+    name: name,
+    email: email,
+    password: password
+  };
 
-    localStorage.setItem("user", JSON.stringify(user));
-    alert("Signup Successful!");
-    showLogin();
-  }
-function clicklogin(){
- window.location.href="login.html"
+  localStorage.setItem("user", JSON.stringify(user));
+  alert("Signup Successful!");
+  showLogin();
 }
-function clicksignup(){
-    window.location.href="signup.html"
+function clicklogin() {
+  window.location.href = "login.html"
 }
-function clickhome(){
-    window.location.href="home.html"
+function clicksignup() {
+  window.location.href = "signup.html"
 }
-  function login() {
-    var email = document.getElementById("loginEmail").value;
-    var password = document.getElementById("loginPassword").value;
+function clickhome() {
+  window.location.href = "home.html"
+}
+function login() {
+  var email = document.getElementById("loginEmail").value;
+  var password = document.getElementById("loginPassword").value;
 
-    var user = JSON.parse(localStorage.getItem("user"));
+  var user = JSON.parse(localStorage.getItem("user"));
 
-    if (user && user.email === email && user.password === password) {
-      alert("Login Successful!");
-      window.location.href = "main.html"; 
-    } else {
-      alert("Invalid Email or Password");
-    }
+  if (user && user.email === email && user.password === password) {
+    alert("Login Successful!");
+    window.location.href = "main.html";
+  } else {
+    alert("Invalid Email or Password");
   }
- 
-  function clicknow(){
-    window.location.href="signup.html"
-  }
-  function guest(){
-    window.location.href="main.html"
-  }
+}
+
+function clicknow() {
+  window.location.href = "signup.html"
+}
+function guest() {
+  window.location.href = "main.html"
+}
 function getData() {
 
   var cvData = {
-    fName: document.getElementById("fName").value,
-    lName: document.getElementById("lName").value,
-    num: document.getElementById("num").value,
-    email: document.getElementById("email").value,
-    address: document.getElementById("address").value,
-    title: document.getElementById("title").value,
-    summary: document.getElementById("summary").value
+    fName: document.getElementById("fName").value.trim(),
+    lName: document.getElementById("lName").value.trim(),
+    num: document.getElementById("num").value.trim(),
+    email: document.getElementById("email").value.trim(),
+    address: document.getElementById("address").value.trim(),
+    title: document.getElementById("title").value.trim(),
+    summary: document.getElementById("summary").value.trim()
   };
+
+
+  if (
+    cvData.fName === "" ||
+    cvData.lName === "" ||
+    cvData.num === "" ||
+    cvData.email === "" ||
+    cvData.address === "" ||
+    cvData.title === "" ||
+    cvData.summary === ""
+  ) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Please fill all fields!",
+      confirmButtonColor: "#ffcc00",
+    });
+    return;
+  }
 
   console.log(cvData);
 
   localStorage.setItem("cvData", JSON.stringify(cvData));
-
-  window.location.href = "template.html";
+  window.location.href = "form.html";
 }
-
 
 function selectTemplate(templateName) {
   localStorage.setItem("selectedTemplate", templateName);
-   window.location.href = "preview.html";
+  window.location.href = "preview.html";
 }
 
 
@@ -87,8 +104,14 @@ window.onload = function () {
         <p>${data.email}</p>
         <p>${data.num}</p>
         <p>${data.address}</p>
+        <h3>Job Title</h3>
         <p>${data.title}</p>
+        <h3>Summary</h3>
         <p>${data.summary}</p>
+        <h3>Education</h3>
+        <p>${data.education || ""}</p>
+        <h3>Skill</h3>
+        <p>${data.skills || ""}</p>
       </div>
     `;
   }
@@ -96,13 +119,80 @@ window.onload = function () {
   if (template === "template2") {
     document.getElementById("cv").innerHTML = `
       <div class="template2">
+      <div class ="left">
         <h2>${data.fName} ${data.lName}</h2>
+          <p>${data.email}</p>
+        <p>${data.num}</p>
+        <p>${data.address}</p>
+        <div class="right">
+         <h3>Job Title</h3>
         <p>${data.title}</p>
         <hr>
+<h3>Summary</h3>
         <p>${data.summary}</p>
-        <p>${data.email}</p>
+        <h3>Education</h3>
+        <p>${data.education || ""}</p>
+        <h3>Skill</h3>
+        <p>${data.skills || ""}</p>
+      </div>
       </div>
     `;
   }
 
 };
+function saveCV() {
+  var cv = {
+    fullname: document.getElementById("fullName"),
+    email: document.getElementById("emailForm").value,
+    phone: document.getElementById("phone").value,
+    title: document.getElementById("title").value,
+    summary: document.getElementById("summary").value,
+    education: document.getElementById("education").value,
+    experience: document.getElementById("experience").value,
+    skills: document.getElementById("skills").value
+  };
+  if (cv.fullname == "" ||
+    cv.emailForm == "" ||
+    cv.phone == "" ||
+    cv.title == "" ||
+    cv.summary == "" ||
+    cv.education == "" ||
+    cv.experience == "" ||
+    cv.skills == ""
+
+  ) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Please fill all fields!",
+      confirmButtonColor: "#ffcc00",
+    });
+    return;
+  }
+
+
+  localStorage.setItem("cvData", JSON.stringify(cv));
+  window.location.href = "template.html"
+}
+var currentStep = 0;
+var steps = document.querySelectorAll(".step");
+
+function showStep(index) {
+  steps.forEach(step => step.classList.remove("active"));
+  steps[index].classList.add("active");
+}
+
+function nextStep() {
+  if (currentStep < steps.length - 1) {
+    currentStep++;
+    showStep(currentStep);
+  }
+}
+
+function prevStep() {
+  if (currentStep > 0) {
+    currentStep--;
+    showStep(currentStep);
+  }
+}
+var
